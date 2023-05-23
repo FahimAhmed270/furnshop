c3922ceddd603125ce9118894c7b7b492ff0f00a
FahimAhmed270 <56820604+FahimAhmed270@users.noreply.github.com>
2023-05-23 17:12:18 +0600

    testminial end

--- src/Component/TestimonialSlider.jsx
+++ src/Component/TestimonialSlider.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { testimonial } from '../data'
 
 const TestimonialSlider = () => {
   return (
-    <div>
-      
-    </div>
+    <Swiper
+      modules={[Navigation, Autoplay]}
+      className='testimonialSlider'
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      navigation
+    >
+      {testimonial.persons.map((person, id) => {
+        const { avatar, name, occupation, message } = person
+        return (
+          <SwiperSlide key={id}>
+            <div className='flex flex-col min-h-[250px] sm:min-h-[200px]'>
+              <div className='flex items-center gap-x-5 mb-5'>
+                {/* avater */}
+                <img src={avatar.type} alt="" />
+                <div>
+                  <div className='text-xl font-semibold'>{name}</div>
+                  <div className='text-gray-500'>{occupation}</div>
+                </div>
+              </div>
+              {/* text */}
+              <div className='text-xl max-w-[550px]'>{message}</div>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
   )
 }
 
